1f9d7f0da310c6e5c09431f96e1750e2fb6a6cd0
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-04-08 20:18:24 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,13 +5,13 @@
         array(
           'menu' => 'Footer Navigation',
           'container'  => 'ul',
-          'menu_class' => 'cell shrink  small-5 align-self-middle no-bullet',
+          'menu_class' => 'cell shrink  small-4 align-self-middle no-bullet',
           'items_wrap' => '<ul id="%1$s" class="%2$s"><li><i class="fi-link"></i></li>%3$s</ul>'
           )
       );?>
           
 
-      <ul class="cell small-7 medium-shrink text-right align-self-middle no-bullet">
+      <ul class="cell small-4 medium-shrink text-right align-self-middle no-bullet">
 
         <li>Portfolio website&nbsp;&copy;</li>
         <?php dynamic_sidebar("widget_one"); ?>
