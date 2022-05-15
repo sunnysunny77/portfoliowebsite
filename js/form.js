a0b6663b1b7c302c43acf2a585a71b66484182f4
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-05-15 11:52:43 +0800

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,74 +1,70 @@
-
 const options_1 = {
-    beforeSubmit:  showRequest_1,
+    beforeSubmit: showRequest_1,
     success: showResponse_1,
     url: frontend_ajax_object.ajaxurl,
 };
 
 const options_2 = {
-    beforeSubmit:  showRequest_2,
+    beforeSubmit: showRequest_2,
     success: showResponse_2,
     url: frontend_ajax_object.ajaxurl,
 };
 
 const options_3 = {
-    beforeSubmit:  showRequest_3,
+    beforeSubmit: showRequest_3,
     success: showResponse_3,
     url: frontend_ajax_object.ajaxurl,
 };
 
-function showRequest_1(formData, jqForm, options) { 
-    const sub = $("#submit-1");
-    sub.prop( "disabled", true );
-    sub.val('Sent');
-} 
+function showRequest_1(formData, jqForm, options) {
+    $("#submit-1").prop("disabled", true);
+}
 
 function showResponse_1(responseText, statusText, xhr, $form) {
     const res = $("#response-1");
     const text = res.text();
     res.html(responseText);
-    setTimeout(function (){
+    const sub = $("#submit-1");
+    sub.val('Sent');
+    setTimeout(function () {
         res.html(text);
-        const sub = $("#submit-1");
-        sub.prop( "disabled", false );
+        sub.prop("disabled", false);
         sub.val('Submit');
         $form.trigger("reset");
     }, 9000);
 }
 
-function showRequest_2(formData, jqForm, options) { 
-    const sub = $("#submit-2");
-    sub.prop( "disabled", true );
-    sub.val('Sent');
-} 
+function showRequest_2(formData, jqForm, options) {
+    $("#submit-2").prop("disabled", true);
+}
 
 function showResponse_2(responseText, statusText, xhr, $form) {
     const res = $("#response-2");
     const text = res.text();
     res.html(responseText);
-    setTimeout(function (){
+    const sub = $("#submit-2");
+    sub.val('Sent');
+    setTimeout(function () {
         res.html(text);
-        const sub = $("#submit-2");
-        sub.prop( "disabled", false );
+        sub.prop("disabled", false);
         sub.val('Submit');
         $form.trigger("reset");
     }, 9000);
 }
 
-function showRequest_3(formData, jqForm, options) { 
-    const sub = $("#submit-3");
-    sub.prop( "disabled", true );
-    sub.val('Sent');
-} 
+function showRequest_3(formData, jqForm, options) {
+    $("#submit-3").prop("disabled", true);
+}
 
 function showResponse_3(responseText, statusText, xhr, $form) {
     const res = $("#response-3");
     const text = res.text();
     res.html(responseText);
-    setTimeout(function (){
+    const sub = $("#submit-3");
+    sub.val('Sent');
+    setTimeout(function () {
         res.html(text);
-        const sub = $("#submit-3");
-        sub.prop( "disabled", false );
+        sub.prop("disabled", false);
         sub.val('Submit');
         $form.trigger("reset");
     }, 9000);
