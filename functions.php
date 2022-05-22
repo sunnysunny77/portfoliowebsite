3d1fdd77318233da2e75d343657d86f2eb13f329
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-05-23 05:07:24 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -316,3 +316,17 @@ function portfolio_website_on_theme_activation()
     }
 }
 add_action('after_switch_theme', 'portfolio_website_on_theme_activation');
+
+function portfolio_website_media_columns($columns) {
+    $columns['post_parent'] = 'Post parent';
+    return $columns;
+} 
+add_filter( 'manage_media_columns', 'portfolio_website_media_columns' );
+ 
+function portfolio_website_media_custom_column($columnName, $columnID){
+    if($columnName == 'post_parent'){
+        $parent = get_post_parent($columnID);
+       echo get_post_type($parent);
+    }
+}
+add_filter( 'manage_media_custom_column', 'portfolio_website_media_custom_column', 10, 2 );
