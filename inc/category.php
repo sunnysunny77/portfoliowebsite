5cb7bbcf56740e83e80bd7f7faf9620c6358102b
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-05-22 23:02:03 +0800

    Update category.php

--- inc/category.php
+++ inc/category.php
@@ -11,6 +11,55 @@ function portfolio_website_category_media()
 }
 add_action('init', 'portfolio_website_category_media');
 
+// remove category field so a user cant enter an incorrect category 
+function portfolio_website_remove_category($fields)
+{
+    unset($fields['category']);
+    return $fields;
+}
+add_filter('attachment_fields_to_edit', 'portfolio_website_remove_category');
+
+// remove media library column so a user cant eddit attachments
+function portfolio_website_columns_manage($columns)
+{
+    unset($columns['parent']);
+    $columns['attached'] = 'Parent';
+    return $columns;
+}
+add_filter('manage_upload_columns', 'portfolio_website_columns_manage');
+
+// add new attachments column
+function portfolio_website_columns_display($column_name, $post_id)
+{
+    if ('attached' == $column_name) {
+        $post = get_post($post_id);
+        $parent =  $post->post_parent;
+
+        if ($parent > 0) {
+
+            $value = '';
+
+            if (get_post($parent)) {
+                $value = "Title: " . _draft_or_post_title($parent) . "<br/> Status: " . get_post_status($parent);
+            }
+
+            if (current_user_can('edit_post', $parent)) {
+?>
+                <a href="<?php echo get_edit_post_link($parent); ?>">
+                    <?php echo  $value; ?>
+                </a>
+<?php
+            } else {
+                echo $value;
+            }
+        } else {
+
+            echo '(Unattached)';
+        }
+    }
+}
+add_action('manage_media_custom_column', 'portfolio_website_columns_display', 10, 2);
+
 // set category after post
 function portfolio_website_after_post_meta($meta_id, $post_id, $meta_key, $meta_value)
 {
@@ -39,8 +88,8 @@ add_action('updated_post_meta', 'portfolio_website_after_post_meta', 10, 4);
 // remove category before trash
 function portfolio_website_trash_post($post_id)
 {
-
     $post_types = ["storyboarding_films", "concepts_films", "independent_films", "theatre", "designs", "poems_poetry", "illustrated_poetry", "sculptures", "illustrations"];
+
     $post = get_post_type($post_id);
 
     foreach ($post_types as  $post_type) {
@@ -57,8 +106,8 @@ add_action('wp_trash_post', 'portfolio_website_trash_post');
 // add category when untashing
 function portfolio_website_untrash_posts($post_id)
 {
-
     $post_types = ["storyboarding_films", "concepts_films", "independent_films", "theatre", "designs", "poems_poetry", "illustrated_poetry", "sculptures", "illustrations"];
+
     $post = get_post_type($post_id);
 
     foreach ($post_types as  $post_type) {
@@ -78,54 +127,3 @@ function portfolio_website_untrash_posts($post_id)
     }
 }
 add_action('untrash_post', 'portfolio_website_untrash_posts');
-
-
-// remove category field so a user cant enter an incorrect category 
-function portfolio_website_remove_category($fields)
-{
-    unset($fields['category']);
-    return $fields;
-}
-add_filter('attachment_fields_to_edit', 'portfolio_website_remove_category');
-
-
-// remove media library column so a user cant eddit attachments
-function portfolio_website_columns_manage($columns)
-{
-    unset($columns['parent']);
-    $columns['attached'] = 'Parent';
-    return $columns;
-}
-add_filter('manage_upload_columns', 'portfolio_website_columns_manage');
-
-// add new attachments column
-function portfolio_website_columns_display($column_name, $post_id)
-{
-    if ('attached' == $column_name) {
-        $post = get_post($post_id);
-        $parent =  $post->post_parent;
-
-        if ($parent > 0) {
-
-            $title = '';
-
-            if (get_post($parent)) {
-                $title = _draft_or_post_title($parent);
-            }
-
-            if (current_user_can('edit_post', $parent)) {
-?>
-                <a href="<?php echo get_edit_post_link($parent); ?>">
-                    <?php echo $title ?>
-                </a>
-<?php
-            } else {
-                echo $title;
-            }
-        } else {
-
-            echo '(Unattached)';
-        }
-    }
-}
-add_action('manage_media_custom_column', 'portfolio_website_columns_display', 10, 2);
