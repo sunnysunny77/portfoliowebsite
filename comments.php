81130620655fce5c5e2b83377398ed26b3165d03
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-06-20 16:17:19 +0800

    Update comments.php

--- comments.php
+++ comments.php
@@ -21,9 +21,10 @@ comment_form(
     'id_form'           => 'commentform',
     'id_submit'         => 'commentsubmit',
     'title_reply'       => 'Leave a comment',
+    'title_reply_to'    => 'Leave a comment to %s',
     'title_reply_before'   => '<h2 id="reply-title" class="comment-reply-title">',
     'title_reply_after'    => '</h2>',
-    'cancel_reply_link' => 'Cancel Commnet',
+    'cancel_reply_link' => ', Cancel Commnet',
     'comment_field' =>  '<p><label for="comment" class="hide"> Comment </label> <textarea placeholder="Start typing..." id="comment"  name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
     'comment_notes_after' => '<p>' .
       'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:' .
