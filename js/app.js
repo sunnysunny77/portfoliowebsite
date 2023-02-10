ad797f8e85ea6b471b45cda6c001f57122a0adaf
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2023-02-11 01:37:23 +0800

    up

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
+function is_touch_enabled() {
+    return ('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0);
+}
+
 $(document).foundation()
-new Vivus(
-    "my-svg-m",
-    {
-        duration: 300,
-        file: path.dir + "/images/template/logo-m.svg",
-    },
-    null
-);
-new Vivus(
-    "my-svg",
-    {
-        duration: 300,
-        file: path.dir + "/images/template/logo.svg",
-    },
-    null
-);
+
 $(".title-bar-title").click(function () {
     $(".fi-list").toggleClass("color");
+});
+
+$(document).ready(function () {
+    new Vivus(
+        "my-svg-m",
+        {
+            duration: 300,
+            file: path.dir + "/images/template/logo-m.svg",
+        },
+        null
+    );
+
+    new Vivus(
+        "my-svg",
+        {
+            duration: 300,
+            file: path.dir + "/images/template/logo.svg",
+        },
+        null
+    );
+  
+    if (!is_touch_enabled()) {
+        $("body").css('background-image', 'url(' + path.dir + '/images/template/body-desktop.jpg)');
+        $("body").css('background-attachment', 'fixed');
+        $("body").css('background-size', 'cover');
+    }
 });
